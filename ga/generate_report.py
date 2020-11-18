872d1ce5fae76d02aa844493f4ffbcf12aba3211
mardzien <mardzien@gmail.com>
2020-11-19 00:18:35 +0100

    fixing data formats

--- ga/generate_report.py
+++ ga/generate_report.py
@@ -16,19 +16,26 @@ def generate_ga_report(id, d_range, filters, file_name):
                             dimensions=dims,
                             metrics=metri,
                             filters=filters)
-    df1 = get_report(response, dims, metri)
+    df1 = get_report(response, dims, metri).astype({'sessions': 'int64', 'users': 'int64', 'pageViews': 'int64',
+                                                    'month': 'int64'})
     dims = ['ga:segment', 'ga:date']
     response = get_response(analytics, VIEW_ID=id, start_date=d_range_splitted[0], end_date=d_range_splitted[1],
                             dimensions=dims,
                             metrics=metri,
                             filters=filters)
-    df2 = get_report(response, dims, metri)
+    df2 = get_report(response, dims, metri).astype({'sessions': 'int64', 'users': 'int64', 'pageViews': 'int64'})
+    df2['date'] = pd.to_datetime(df2['date'], format='%Y%m%d')
     dims = ['ga:segment', 'ga:month']
     response = get_response(analytics, VIEW_ID=id, start_date=d_range_splitted[0], end_date=d_range_splitted[1],
                             dimensions=dims,
                             metrics=metri,
                             filters=filters)
-    df3 = get_report(response, dims, metri)
+    df3 = get_report(response, dims, metri).astype({'sessions': 'int64', 'users': 'int64', 'pageViews': 'int64',
+                                                    'month': 'int64'})
+
+    print(df1.dtypes)
+    print(df2.dtypes)
+    print(df3.dtypes)
 
     with pd.ExcelWriter(f"Output/{file_name}.xlsx") as writer:
         df1.to_excel(writer, sheet_name='Miesiąc i URL')
