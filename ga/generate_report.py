96a9a6d625e9aa7281666b5f6c0eb01457f21692
mardzien <mardzien@gmail.com>
2020-11-20 15:50:53 +0100

    generating full report

--- ga/generate_report.py
+++ ga/generate_report.py
@@ -3,7 +3,7 @@ import pandas as pd
 from ga.ga_by_filters import initialize_analyticsreporting, get_response, get_report
 
 ID_PROJECT = '168176938'
-date_range = "2020-09-01:2020-09-30"
+date_range = "2020-10-01:2020-10-31"
 FILTER = ['/ru/tipstricks-ru/', '/ru/trendy-ru/', '/тренди/', '/поради-та-рекомендації/']
 
 
@@ -43,4 +43,11 @@ def generate_ga_report(id, d_range, filters, file_name):
         df3.to_excel(writer, sheet_name='Miesiące')
 
 
-generate_ga_report(ID_PROJECT, date_range, FILTER, "born")
+def main():
+    generate_ga_report(ID_PROJECT, date_range, FILTER[:2], "Born RU - raport")
+    generate_ga_report(ID_PROJECT, date_range, FILTER[2:], "Born UA - raport")
+    generate_ga_report(ID_PROJECT, date_range, FILTER, "Born RU + UA - raport")
+
+
+if __name__ == '__main__':
+    main()
